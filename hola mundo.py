86f456dbd56d3989cccbb78519bb03ee49fb3c27
AlexVillanueva23 <105958660+AlexVillanueva23@users.noreply.github.com>
2022-05-30 21:48:54 -0500

    initial commit

---
+++ hola mundo.py
@@ -0,0 +1,6 @@
+calificacion = input("introduce tu calificaion de la az-900 ")
+calificacion = int(calificacion)
+if calificacion < 700:
+    print("estas reprobado") 
+else:
+    print("felicidades pasaste")
